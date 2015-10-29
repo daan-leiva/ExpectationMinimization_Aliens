7365195599a7a2ef5efbb177188b58a273e61dd4
unknown <dleiva@ATI-PC063.ati.local>
2015-10-29 14:29:02 -0700

    Initial commit

---
+++ DataPointsAnalysis/Point.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataPointsAnalysis
+{
+    class Point
+    {
+        double x;
+        double y;
+        double z;
+
+        public Point(double _x, double _y, double _z)
+        {
+            this.x = _x;
+            this.y = _y;
+            this.z = _z;
+        }
+    }
+}
---
+++ DataPointsAnalysis/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataPointsAnalysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Point> points = new List<Point>();
+            List<double> distribution = new List<double>();
+
+            // read in points
+            string points_txt_file = "";
+            using(System.IO.StreamReader reader = new System.IO.StreamReader(points_txt_file))
+            {
+                string[] line;
+                while(!reader.EndOfStream)
+                {
+                    line = reader.ReadLine().Split(new char[] { ' ' });
+                    points.Add(new Point(double.Parse(line[0]), double.Parse(line[1]), double.Parse(line[2]));
+                }
+            }
+
+            // read in distribution
+            string distribution_txt_file = "";
+            using(System.IO.StreamReader reader = new System.IO.StreamReader(distribution_txt_file))
+            {
+                while(!reader.EndOfStream)
+                    distribution.Add(double.Parse(reader.ReadLine()));
+            }
+        }
+    }
+}
